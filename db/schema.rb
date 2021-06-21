f053c65a142b30303e70446eb68dd95e559849a9
Zappat0n <albarros1973@gmail.com>
2021-06-21 18:50:39 +0200

    Fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,25 +12,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_131545) do
-
+ActiveRecord::Schema.define(version: 20_210_621_155_029) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "webhook_endpoints", force: :cascade do |t|
-    t.string "url", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'webhook_endpoints', force: :cascade do |t|
+    t.string 'url', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.jsonb 'subscriptions', default: ['*']
   end
 
-  create_table "webhook_events", force: :cascade do |t|
-    t.integer "webhook_endpoint_id", null: false
-    t.string "event", null: false
-    t.text "payload", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.jsonb "response", default: {}
-    t.index ["webhook_endpoint_id"], name: "index_webhook_events_on_webhook_endpoint_id"
+  create_table 'webhook_events', force: :cascade do |t|
+    t.integer 'webhook_endpoint_id', null: false
+    t.string 'event', null: false
+    t.text 'payload', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.jsonb 'response', default: {}
+    t.index ['webhook_endpoint_id'], name: 'index_webhook_events_on_webhook_endpoint_id'
   end
-
 end
--- test/models/webhook_endpoint_test.rb
+++ test/models/webhook_endpoint_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class WebhookEndpointTest < ActiveSupport::TestCase
   # test "the truth" do
